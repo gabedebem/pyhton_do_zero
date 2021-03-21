f7ad1382e1c607f82316f875b02f767048187c1a
gabedebem <65568583+gabedebem@users.noreply.github.com>
2021-03-21 15:51:02 -0300

    Update exercicio_desafio_media_podenrada.py

--- exercicio_desafio_media_podenrada.py
+++ exercicio_desafio_media_podenrada.py
@@ -1,12 +1,15 @@
 #-*-coding: utf_8-*-
 
 numero_1 = float(input("Digite um numero qualquer:"))
-numero_2 = float(input("Digite outro numero qualquer:"))
+peso_numero_1 = float(input("Digite o peso para o primeiro número"))
+numero_2 = float(input("Digite o segundo numero:"))
+peso_numero_2 = float(input("Digite o peso para o segundo número"))
 numero_3 = float(input("Digite o terceiro numero:"))
+peso_numero_3 = float(input("Digite o peso para o terceiro número"))
 
 denominador = numero_1+numero_2+numero_3
-numerador = (numero_1*2)+(numero_2*3)+(numero_3*5)
-denominador = 2+3+5
+numerador = (numero_1*peso_numero_1)+(numero_2*peso_numero_2)+(numero_3*peso_numero_3)
+denominador = peso_numero_1+peso_numero_2+peso_numero_3
 
 media_ponderada = numerador/denominador
 print(f"A media ponderada é {media_ponderada}")
